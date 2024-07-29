f5c71c860e7eb0d09e783c3eec5432116a67c4ce
Rohanwagh5 <129928120+Rohanwagh5@users.noreply.github.com>
2024-07-29 16:54:30 +0530

    Add files via upload

---
+++ src/components/TechIcons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaCode } from 'react-icons/fa';
+
+function TechIcons() {
+  return (
+    <div>
+      <FaCode size={80} title="C++" />
+    </div>
+  );
+}
+
+export default TechIcons;
---
+++ src/index.js
@@ -0,0 +1,54 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { ToastContainer } from 'react-toastify';
+// import "react-toastify/dist/ReactToastify.css";
+// import { BrowserRouter } from 'react-router-dom';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+  
+//   <BrowserRouter>
+//        <App/>
+//        <ToastContainer></ToastContainer>
+//   </BrowserRouter>
+
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
